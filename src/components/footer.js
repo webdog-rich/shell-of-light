ff5006bfc79517f32f9484735abd65bb557fe6bd
dontbetriangle <richard.legg.89@gmail.com>
2021-08-05 18:40:03 +1200

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,16 @@
 import React from "react"
 
-const Footer = () => {
+import { convertToBgImage } from "gbimage-bridge"
+import BackgroundImage from "gatsby-background-image"
+
+const Footer = ({ heroImage }) => {
+  const bgImage = convertToBgImage(heroImage)
+
   return (
-    <footer className="footer">
+    <footer>
       <div className="content has-text-centered">
         <strong>
-          Designed by <a href="https://www.webdog.co.nz">Webdog</a>
+          Built by <a href="https://www.webdog.co.nz">Webdog</a>
         </strong>
       </div>
     </footer>
