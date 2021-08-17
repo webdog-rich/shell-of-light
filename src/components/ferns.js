ec37a18fa2ad0af1f31085095d896613dcd58f04
webdog-rich <richard@webdog.co.nz>
2021-08-17 17:04:42 +1200

    testing page-transitions

--- src/components/ferns.js
+++ src/components/ferns.js
@@ -4,23 +4,8 @@ import { StaticImage } from "gatsby-plugin-image"
 
 export default function Ferns() {
   return (
-    <div style={{ position: "absolute", width: "100%" }}>
-      <StaticImage
-        src="../images/ferns-design-v2-left.png"
-        alt="Beautiful Fern"
-        style={{ left: "0px", top: "0px", position: "absolute" }}
-        className="left-fern fern"
-        objectFit="contain"
-        placeholder="none"
-      />
-      <StaticImage
-        src="../images/ferns-design-v2-right.png"
-        alt="Beautiful Fern"
-        style={{ right: "-15px", top: "0px", position: "absolute" }}
-        className="right-fern fern"
-        objectFit="contain"
-        placeholder="none"
-      />
+    <div style={{ position: "fixed", top: "0px", zIndex: 9999999 }}>
+      <StaticImage src="../images/fern.jpg" alt="A Fern" />
     </div>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Ferns from "./ferns"
 import Footer from "./footer"
 import Hero from "./hero"
+import { motion } from "framer-motion"
 
 const Layout = ({
   children,
@@ -12,15 +13,23 @@ const Layout = ({
 }) => {
   return (
     <>
-      {/* <Ferns /> */}
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        exit={{ opacity: 1 }}
+        transition={{ duration: 5 }}
+        style={{ zIndex: 999999 }}
+      >
+        <Ferns />
+      </motion.div>
       <Hero
         heroImage={heroImage}
         pageTitle={pageTitle}
         isLarge={largeHero ? true : false}
         pageSubTitle={pageSubTitle}
       />
-
       {children}
+
       <Footer />
     </>
   )
